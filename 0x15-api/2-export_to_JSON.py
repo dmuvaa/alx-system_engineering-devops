542bd9b23e06489904abd660c8fe20b2f7442675
Dennis Muvaa <dmuvaa70@gmail.com>
2023-07-26 17:40:53 +0300

    Extend Python script to export data in the JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,36 +10,35 @@ if len(sys.argv) < 2:
     print("Error: No employee ID provided.")
     sys.exit(1)
 
-    try:
-        user_id = int(sys.argv[1])
-    except ValueError:
-        print("Error : Employee ID must be an integer.")
-        sys.exit(1)
+try:
+    user_id = int(sys.argv[1])
+except ValueError:
+    print("Error : Employee ID must be an integer.")
+    sys.exit(1)
 
-    todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    todos_response = requests.get(tools_url)
-    user_response = requests.get(user_url)
+todos_url = 'https://jsonplaceholder.typicode.com/todos'
+user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+todos_response = requests.get(todos_url)
+user_response = requests.get(user_url)
 
-    if todos_response.status_code != 200 or user_response.status_code != 200:
-        print(f"Error: Unable to get data for employee with ID {user_id}")
-        sys.exit(1)
+if todos_response.status_code != 200 or user_response.status_code != 200:
+    print(f"Error: Unable to get data for employee with ID {user_id}")
+    sys.exit(1)
 
-    todos_data = todos_response.json()
-    user_data = user_response.json()
+todos_data = todos_response.json()
+user_data = user_response.json()
 
-    tasks = []
+tasks = []
 
-    for todo in todos_data:
-        if todo['userId'] == user_id:
-            task = {
-                    'task': todo['title'],
-                    'completed' : todo['completed'],
-                    'username': user_data['username']
+for todo in todos_data:
+    if todo['userId'] == user_id:
+        task = {
+                'task': todo['title'],
+                'completed': todo['completed'],
+                'username': user_data['username']
 
-            }
-            tasks.append(task)
+        }
+        tasks.append(task)
 
-    with open(f'{user_id}.json', 'w') as json_file:
-        json.dump({str(user_id): tasks}, json_file)
-                
+with open(f'{user_id}.json', 'w') as json_file:
+    json.dump({str(user_id): tasks}, json_file)
