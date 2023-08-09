07ef78157be6778702d9ce46293db6c63f0a551c
Dennis Muvaa <dmuvaa70@gmail.com>
2023-08-09 17:34:57 +0300

    recursive function that queries the Reddit API, parses the title of all hot articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
-
-"""imports a module"""
-
+"""Module for task 3"""
 import requests
 
-
-def count_words(subreddit, word_list, word_count={}, after=None):
+def count_words(subreddit, word_list, word_count=None, after=None):
     """Queries the Reddit API and returns the count of words in
     word_list in the titles of all the hot posts
     of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
+    
+    if word_count is None:
         word_count = {word: 0 for word in word_list}
+        
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "My-User-Agent"}
+    
+    response = requests.get(url, headers=headers, params={"after": after}, allow_redirects=False)
 
-    for title in hot_l:
-        split_words = title.split(' ')
+    if response.status_code != 200:
+        return
+
+    data = response.json()
+
+    titles = [child["data"]["title"] for child in data["data"]["children"]]
+    
+    for title in titles:
+        split_words = title.lower().split(' ')
         for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
+            word_count[word] += split_words.count(word.lower())
 
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+    next_page = data["data"].get("after")
+    
+    if not next_page:
+        sorted_counts = sorted(word_count.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+        for k, v in sorted_counts:
+            if v != 0:
+                print(f'{k}: {v}')
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        count_words(subreddit, word_list, word_count, next_page)
