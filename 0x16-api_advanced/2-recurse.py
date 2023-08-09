106d645411418c3864190366c4236ffdfbe63301
Dennis Muvaa <dmuvaa70@gmail.com>
2023-08-09 15:31:31 +0300

    recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ import json
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """recursive function that queries the Reddit API
     and returns a list containing the titles of all hot articles
     for a given subreddit"""
