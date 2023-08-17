d5c1ba4156d1f4709344b197e13e4a249aef8939
Dennis Muvaa <dmuvaa70@gmail.com>
2023-08-17 18:58:25 +0300

    Webstack Debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# increase the ulimit 
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+#restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
